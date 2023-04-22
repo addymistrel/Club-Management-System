3c3f608f42f8ecc4ef25279188db335a83a1efdc
addymistrel <addymistrel@gmail.com>
2023-04-22 17:25:23 +0530

    all done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,29 +23,22 @@ const navbar = () => {
             </li>
             <li>
               <Link to="services" spy={true} smooth={true}>
-                Serivces
+                About
               </Link>
             </li>
             <li>
               <Link to="works" spy={true} smooth={true}>
-                Experience
+                Clubs
               </Link>
             </li>
             <li>
-              <Link to="portfolio" spy={true} smooth={true}>
-                Protfolio
-              </Link>
-            </li>
-            <li>
-              <Link to="testimonial" spy={true} smooth={true}>
-                Testimonial
+              <Link to="contact" spy={true} smooth={true}>
+                Contact
               </Link>
             </li>
           </ul>
         </div>
-        <Link to="contact" spy={true} smooth={true}>
-        <button className="button n-button">Contact</button>
-        </Link>
+        <button className="button n-button">Login</button>
       </div>
     </div>
   );
