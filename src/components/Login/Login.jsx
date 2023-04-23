8878eee5a30d7e0bfb582fea65058850ba8eca8e
addymistrel <addymistrel@gmail.com>
2023-04-23 15:54:27 +0530

    4 pm done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Helmet} from 'react-helmet';
 import "./Login.css"
+import pic from "../../img/bglogin.jpg"
 
 const Login = ()=> {
   return (
@@ -8,12 +9,13 @@ const Login = ()=> {
     <Helmet>
         <link  href="./Login.css" rel="stylesheet"/>
     </Helmet>
+    <img className="imgbg" src={pic} alt="a"/>
     <div className="login" id="login">
   <h2>Login</h2>
   <form>
     <div className="user-box">
       <input type="text" name="" required=""/>
-      <label>Username</label>
+      <label>Username </label>
     </div>
     <div className="user-box">
       <input type="password" name="" required=""/>
